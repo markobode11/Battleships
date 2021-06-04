125d1136d8639f8408943719458df083e482802d
Bode <mabode@ttu.ee>
2021-06-04 10:42:11 +0300

    Boat seed.

--- Battleships/WebApp/Pages/Index.cshtml.cs
+++ Battleships/WebApp/Pages/Index.cshtml.cs
@@ -1,8 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Domain;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly DAL.AppDbContext _context;
+        
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task OnGetAsync()
+        {
+            if (!_context.Boats.Any())
+            {
+                var names = new List<string>()
+                {
+                    "Patrol",
+                    "Submarine",
+                    "Battleship",
+                    "Destroyer",
+                    "Cruiser"
+                };
+                for (int i = 0; i < 5; i++)
+                {
+                    _context.Boats.Add(new Boat
+                    {
+                        Name = names[i],
+                        Size = i + 1
+                    });
+                }
+
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
---
+++ Battleships/WebApp/Pages/SetNames/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Pages.SetNames
+{
+    public class Index : PageModel
+    {
+        private readonly DAL.AppDbContext _context;
+
+        public Index(DAL.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty(SupportsGet = true)] public int GameId { get; set; }
+        [BindProperty] public Game Game { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var game = await _context.Games
+                .Where(x => x.GameId == GameId)
+                .Include(x => x.GameOption)
+                .ThenInclude(option => option!.GameOptionBoats)
+                .Include(x => x.Player1)
+                .Include(x => x.Player2)
+                .FirstAsync();
+
+            game.Description = Game.Description;
+            game.Player1.Name = Game.Player1.Name;
+            game.Player2.Name = Game.Player2.Name;
+            await _context.SaveChangesAsync();
+
+            //If no optionBoats were chosen redirect to WinPage and display player1 as winner.
+            var total = 0;
+
+            foreach (var optionBoat in game.GameOption!.GameOptionBoats)
+            {
+                total += optionBoat.Amount;
+            }
+
+            return Redirect(total == 0
+                ? $"/BoardPage/Index?GameId={GameId}"
+                : $"/PlaceBoatsPage/Index?GameId={GameId}&BoatsPlaced=0&P1Turn=true");
+        }
+    }
+}
